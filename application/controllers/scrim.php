0a3e0590aae2332fbaa2243c907be513652ce775
RinghusDK <michael@ringhus.dk>
2018-01-18 10:04:30 +0100

    Created scrim class file

---
+++ application/controllers/scrim.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Scrim extends CI_Controller {
+
+    public function __construct() {
+
+
+
+    }
+
+
+    public function finder() {
+
+
+    }
+
+
+    public function create() {
+
+
+
+    }
+
+
+    public function view() {
+
+
+
+    }
+
+    public function delete() {
+
+
+    }
+
+}
