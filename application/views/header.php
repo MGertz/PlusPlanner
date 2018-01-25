c79f3992383076a77d52b29c1cb8740a9d9610a8
RinghusDK <michael@ringhus.dk>
2018-01-25 14:20:59 +0100

    created html for profile page

--- application/views/header.php
+++ application/views/header.php
@@ -1,49 +1,58 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <!-- <link rel="stylesheet" href="/css/bootstrap/4.0.0-beta.3/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous"> -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
-
-
-    <!-- PlusPlanner CSS FIle -->
-    <link rel="stylesheet" href="/css/plusplanner.css">
-
-    <title><?php echo $sitetitle; ?></title>
-  </head>
-  <body>
+		<!-- Bootstrap CSS -->
+		<!-- <link rel="stylesheet" href="/css/bootstrap/4.0.0-beta.3/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous"> -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
 
 
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-    <a class="navbar-brand" href="#">PlusPlanner</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+		<!-- PlusPlanner CSS FIle -->
+		<link rel="stylesheet" href="/css/plusplanner.css">
 
-        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-          <li class="nav-item active">
-            <a class="nav-link" href="/">Forside</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/scrim/finder">Scrim Finder</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/team/index">Team</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/profile">Profile</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/user/logout">Logout</a>
-          </li>
-        </ul>
+		<title><?php echo $sitetitle; ?></title>
 
-      </div>
-    </div>
+		
+		<meta http-equiv="cache-control" content="max-age=0" />
+		<meta http-equiv="cache-control" content="no-cache" />
+		<meta http-equiv="expires" content="0" />
+		<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+		<meta http-equiv="pragma" content="no-cache" />
+
+
+	</head>
+	<body>
+
+
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<div class="container">
+		<a class="navbar-brand" href="#">PlusPlanner</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+
+				<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+					<li class="nav-item active">
+						<a class="nav-link" href="/">Forside</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/scrim/finder">Scrim Finder</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/team/index">Team</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/profile">Profile</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/user/logout">Logout</a>
+					</li>
+				</ul>
+
+			</div>
+		</div>
 </nav>
--- application/views/profile/index.php
+++ application/views/profile/index.php
@@ -1,5 +1,77 @@
 <div class="container box-outer">
+	<div class="row">
 
-Profile Index page
+		<div class="col-sm-3 profile_sidebar_left">
+		
+			<div class="profile_userpic"><img src="https://i.imgur.com/RoMEgQW.jpg" class="img-responsive" alt=""></div>
 
+		
+		
+			<div class="row">
+				<table class="profile_table">
+					<tbody>
+						<tr>
+							<td class="profile_table_left">Name:</td>
+							<td class="profile_table_right">Firstname Lastname</td>
+						</tr>
+						<tr>
+							<td class="profile_table_left">Birthday:</td>
+							<td class="profile_table_right">32. Januar 1954</td>
+						</tr>
+						<tr>
+							<td class="profile_table_left">Country:</td>
+							<td class="profile_table_right"><img src="https://lipis.github.io/flag-icon-css/flags/4x3/dk.svg" class="profile_flag"> Denmark</td>
+						</tr>
+						<tr>
+							<td class="profile_table_left">Status:</td>
+							<td class="profile_table_right">Online</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		
+		
+		
+		
+		
+		
+		
+		</div>
+
+
+		<div class="col-sm-6">
+				<h2>Lidt om mig:</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lobortis id risus nec laoreet. Cras at sapien sed est dignissim gravida et vitae ante. Donec in tempor mauris. Nullam nec nisl vel lectus hendrerit posuere. Nullam scelerisque sed augue id tristique. Sed mollis non ante ac ornare. In feugiat erat eu libero vehicula congue. Vestibulum vehicula vestibulum ultricies. Proin tellus urna, laoreet quis finibus quis, scelerisque eu eros. Fusce lacinia lacinia odio nec sollicitudin. In mollis justo leo, quis dictum ipsum hendrerit ut. Sed malesuada, dolor quis mollis convallis, erat velit porttitor enim, vel tincidunt diam neque eget arcu. Nunc convallis quam felis, in ullamcorper ante pretium nec. In felis purus, suscipit non euismod nec, molestie quis nibh. Morbi congue mattis dui, eget placerat dolor luctus eget. Ut ac elit sit amet felis ultricies tincidunt.</p>
+				<p>Aenean aliquam commodo diam, sit amet consectetur elit. Fusce laoreet, erat sed porttitor congue, leo ex sollicitudin enim, sit amet fermentum massa sapien eget libero. Nunc vel sem ornare, fermentum orci ut, lacinia elit. Ut malesuada condimentum elementum. Fusce non sodales dolor, cursus sollicitudin mauris. Pellentesque non nunc eu massa euismod aliquam in ac libero. Phasellus faucibus ex nec velit ultrices aliquam. Donec congue neque eu mauris blandit suscipit. Nullam et augue a lacus molestie iaculis ac nec enim. Nulla condimentum dolor at erat dictum egestas.</p>
+		</div>
+
+
+
+		<div class="col-sm-3 profile_sidebar_right">
+			<h3 class="center">Most played Heroes</h3>
+			<div class="row">
+				<div class="col-sm-4 profile_most_played_heroes"><img src="https://d1u1mce87gyfbn.cloudfront.net/hero/mercy/hero-select-portrait.png"><p class="profile_most_played_heroes_name">Mercy</p></div>
+				<div class="col-sm-4 profile_most_played_heroes"><img src="https://d1u1mce87gyfbn.cloudfront.net/hero/lucio/hero-select-portrait.png"><p class="profile_most_played_heroes_name">Lucio</p></div>
+				<div class="col-sm-4 profile_most_played_heroes"><img src="https://d1u1mce87gyfbn.cloudfront.net/hero/zenyatta/hero-select-portrait.png"><p class="profile_most_played_heroes_name">Zenyatta</p></div>
+			</div>
+		
+
+			<h3 class="center">Follow me</h3>
+			<div class="row">
+				<div class="col-sm-12 center">
+					<a href="/profile" target="_blank"><img src="http://home.ringhus.dk/plusplanner-files/social-icons/youtube.png" class="profile_social_icon"></a>
+					<a href="/profile" target="_blank"><img src="http://home.ringhus.dk/plusplanner-files/social-icons/facebook.png" class="profile_social_icon"></a>
+					<a href="/profile" target="_blank"><img src="http://home.ringhus.dk/plusplanner-files/social-icons/twitch.png" class="profile_social_icon"></a>
+
+				</div>
+			</div>
+
+				
+		</div>
+
+
+
+
+
+	</div> 
 </div>
